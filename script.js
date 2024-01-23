f799398b23bc7a0563984d09eb72e79da89f15ec
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-23 10:11:28 -0800

    Establish alphabet array

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.log(`Testing is here`);
 
-
-
 function sum(a, b) {
   return a + b;
 }
@@ -31,77 +29,92 @@ calculator.add = function (a, b) {
   return a + b;
 };
 
-calculator.subtract = function(a, b){
-    return a - b;
-}
+calculator.subtract = function (a, b) {
+  return a - b;
+};
 
-calculator.divide = function(a, b){
-    let result = a / b;
-    return result;
-}
+calculator.divide = function (a, b) {
+  let result = a / b;
+  return result;
+};
 
-calculator.multiply = function(a, b){
-    let result = a * b;
-    return result;
-}
+calculator.multiply = function (a, b) {
+  let result = a * b;
+  return result;
+};
 
 // **********************************************************************
 // caeserCipher
 
-function caesarCipher(string){
+function caesarCipher(string) {}
 
-}
-
-let testString = "John"
-console.log(testString)
+let testString = "JoHn";
+testString = testString.toLowerCase()
+console.log(testString);
 let splitString = testString.split("");
 console.log(splitString);
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
 let alphabetKey = {
-    "a": 1,
-    "b": 2,
-    "c": 3,
-    "d": 4,
-    "e": 5,
-    "f": 6,
-    "g": 7,
-    "h": 8,
-    "i": 9,
-    "j": 10,
-    "k": 11,
-    "l": 12,
-    "m": 13,
-    "n": 14,
-    "o": 15,
-    "p": 16,
-    "q": 17,
-    "r": 18,
-    "s": 19,
-    "t": 20,
-    "u": 21,
-    "v": 22,
-    "w": 23,
-    "x": 24,
-    "y": 25,
-    "z": 26,
-}
-
-
-console.log(alphabetKey.e)
-console.log(alphabetKey.find(5))
-
-
-
-
-
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
+};
 
 module.exports = {
   sum,
   capitalize,
   reverseString,
   calculator,
-  caesarCipher
+  caesarCipher,
 };
-
-
-
